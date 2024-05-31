66f99417d91f89a21cc26eb81b6cbb3969b693d8
Vladimir Nazarenko <vnazaren@yandex.ru>
2024-06-01 00:59:17 +0300

    The first part of the diploma project is ready for second review.

--- stat-service/stat-client/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ stat-service/stat-client/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -3,25 +3,12 @@ package ru.practicum.ewm.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
-
-    @ExceptionHandler({ConstraintViolationException.class,
-            MethodArgumentNotValidException.class,
-            IllegalArgumentException.class})
-    public ResponseEntity<ErrorMessage> handleValidation(Exception e) {
-        log.info("HttpStatus.BAD_REQUEST (400). {}", e.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorMessage(e.getMessage()));
-    }
 
     @ExceptionHandler
     public ResponseEntity<ErrorMessage> handleThrowable(final Throwable t) {
