738927f8087e5b93258b6f84fd3ae5439efaa026
Vladimir Nazarenko <vnazaren@yandex.ru>
2024-06-14 01:28:19 +0300

    Fix a problem with the statistics server.

--- stat-service/stat-server/src/main/java/ru/practicum/ewm/storage/HitRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/ewm/storage/HitRepository.java
@@ -15,41 +15,41 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     // тупо все
-    @Query(value = "select new ru.practicum.ewm.model.Stat( h.app, h.uri, count(distinct h.ipAddress)) " +
+    @Query(value = "select new ru.practicum.ewm.model.Stat( h.app, h.uri, count(h.ipAddress)) " +
             "from Hit as h " +
             "where h.hitDate between :start and :end " +
             "group by h.app, h.uri " +
-            "order by count(distinct h.ipAddress) desc")
+            "order by count(h.ipAddress) desc")
     List<Stat> getHits(@Param("start") LocalDateTime start,
                        @Param("end") LocalDateTime end);
 
     // тупо все и считаем уникальные IP
-    @Query(value = "select new ru.practicum.ewm.model.Stat(h.app, h.uri, count(h.ipAddress)) " +
+    @Query(value = "select new ru.practicum.ewm.model.Stat(h.app, h.uri, count(distinct h.ipAddress)) " +
             " from Hit as h " +
             " where h.hitDate between :start and :end " +
             " group by h.app, h.uri " +
-            " order by count(h.ipAddress) desc ")
+            " order by count(distinct h.ipAddress) desc ")
     List<Stat> getUniqueHits(@Param("start") LocalDateTime start,
                              @Param("end") LocalDateTime end);
 
     // по списку URI
+    @Query(value = "select new ru.practicum.ewm.model.Stat(h.app, h.uri, count(h.ipAddress)) " +
+            " from Hit as h " +
+            " where h.hitDate between :start and :end " +
+            "   and h.uri in :uris " +
+            " group by h.app, h.uri " +
+            " order by count(h.ipAddress) desc ")
+    List<Stat> getHitsByUris(@Param("uris") List<String> uris,
+                             @Param("start") LocalDateTime start,
+                             @Param("end") LocalDateTime end);
+
+    // по списку URI и уникальные IP
     @Query(value = "select new ru.practicum.ewm.model.Stat(h.app, h.uri, count(distinct h.ipAddress)) " +
             " from Hit as h " +
             " where h.hitDate between :start and :end " +
             "   and h.uri in :uris " +
             " group by h.app, h.uri " +
             " order by count(distinct h.ipAddress) desc ")
-    List<Stat> getHitsByUris(@Param("uris") List<String> uris,
-                             @Param("start") LocalDateTime start,
-                             @Param("end") LocalDateTime end);
-
-    // по списку URI и уникальные IP
-    @Query(value = "select new ru.practicum.ewm.model.Stat(h.app, h.uri, count(h.ipAddress)) " +
-            " from Hit as h " +
-            " where h.hitDate between :start and :end " +
-            "   and h.uri in :uris " +
-            " group by h.app, h.uri " +
-            " order by count(h.ipAddress) desc ")
     List<Stat> getUniqueHitsByUris(@Param("uris") List<String> uris,
                                    @Param("start") LocalDateTime start,
                                    @Param("end") LocalDateTime end);
